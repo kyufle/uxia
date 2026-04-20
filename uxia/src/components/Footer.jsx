dc144268dee40e442ea3ccbecfbeca0f064595b3
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-04-20 16:09:22 +0200

    change url footer photos and add uxia logo

--- uxia/src/components/Footer.jsx
+++ uxia/src/components/Footer.jsx
@@ -4,10 +4,10 @@ import gobiernoEspana from '../../src/assets/footer logos/gobiernoEspana.png'
 export default function Footer() {
     return (
         <footer className='shadow-inner flex items-center gap-4 p-4 sm:p-8 sm:w-full flex-row justify-between container mx-auto xl:max-w-none xl:w-full xl:px-20'>
-            <a href="https://web.gencat.cat/ca/ciutadania/inici">
+            <a href="https://sites.google.com/xtec.cat/proyectos-de-innovacion/inicio">
                 <img className='w-1/1 xl:w-80' src={gene} alt="Logo Generalitat de Catalunya" />
             </a>
-            <a href="https://www.educacionfpydeportes.gob.es/mc/sgctie/cooperacion-territorial/fondo-social-europeo.html">
+            <a href="https://www.boe.es/boe/dias/2023/09/01/pdfs/BOE-B-2023-24805.pdf">
                 <img className='w-1/1 xl:w-80' src={gobiernoEspana} alt="Logo Govern d'Espanya" />
             </a>
         </footer>
--- uxia/src/components/Header.jsx
+++ uxia/src/components/Header.jsx
@@ -1,7 +1,8 @@
+import uxiaLogo from '../assets/uxiaLogo.png'
 export default function Header() {
     return (
-        <div className='shadow-md flex items-center gap-4 p-4 sm:p-8 flex-row justify-between container mx-auto xl:max-w-none xl:w-full'>
-            <p>uxia</p>
+        <div className='shadow-md flex items-center gap-4 p-4 sm:p-8 flex-row justify-left container mx-auto xl:max-w-none xl:w-full'>
+            <img src={uxiaLogo} className='w-20' alt="Logo d'uxia"/><p>uxia</p>
         </div>
     );
 }

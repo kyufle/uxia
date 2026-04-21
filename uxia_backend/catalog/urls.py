cc4700922e7e6c70085cb7a26b3ea498e53c83be
miguel <mangell0624@gmail.com>
2026-04-21 18:36:16 +0200

    complete spec11

--- uxia_backend/catalog/urls.py
+++ uxia_backend/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_coches
-from .views import get_expo
+from .views import get_coches, get_expo, foto_maria
+
 urlpatterns = [
     path('coches/', get_coches, name='get_coches'),
     path('expo/', get_expo, name='get_expo'),
+    path('foto/', foto_maria, name='foto_maria'),
 ]
--- uxia_backend/uxia_backend/settings.py
+++ uxia_backend/uxia_backend/settings.py
@@ -48,15 +48,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog',
-    'rest_framework'
+    'catalog'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # <--- El primero siempre
+    'django.middleware.common.CommonMiddleware', # <--- Debe ir justo después de CorsMiddleware
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -118,15 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split(",")
 CORS_ALLOW_ALL_ORIGINS = True

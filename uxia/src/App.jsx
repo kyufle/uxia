ef1cd09d70381b4f30c56cd0f6200490e00237e0
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-04-21 20:04:55 +0200

    setup.sh funciona y app no tiene bug

--- uxia/src/App.jsx
+++ uxia/src/App.jsx
@@ -16,18 +16,12 @@ function App() {
       <main className='flex-grow flex flex-col items-center justify-center w-full bg-gray-50 pb-10'>
         {!showCamera && (
         <>
-            <div className='w-full max-w-xs md:max-w-md xl:w-200 xl:px-20 mt-10'>
-              <SelectExpo seleccionado={seleccionado} setSeleccionado={setSeleccionado} />
-            </div>
-            <div className="my-8 p-3 w-full max-w-xs md:max-w-md xl:max-w-full xl:px-10 animate-in fade-in duration-500">
-              {!seleccionado ? (
-                <p className='sm:text-center text-gray-400 font-medium'> 
-                  No has seleccionat cap exposició
-                </p>
-              ) : (
-                <Carrousel photos={mockImagenes}/>
-              )}
-            </div>
+ <div className='w-full max-w-xs md:max-w-md xl:w-200 xl:px-20'>
+          <SelectExpo seleccionado={seleccionado} setSeleccionado={setSeleccionado} />
+        </div>
+        <div className="my-8 p-3 w-full max-w-xs md:max-w-md xl:max-w-full xl:px-10">
+          {!seleccionado ? <p className='sm:text-center'> No has seleccionat cap exposició</p> : <Carrousel seleccionado={seleccionado}/>}
+        </div>
           </>
         )}
         <div className='w-full max-w-xs md:max-w-md xl:w-200'>

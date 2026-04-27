0496ac678a4801d9bca12fd7926f6f375300179c
Miguel Ángel López González <miguelangellopezgonzalez@MacBook-Air-de-Miguel.local>
2026-04-27 18:04:29 +0200

    add url

--- uxia_backend/catalog/urls.py
+++ uxia_backend/catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_coches, get_expo, get_coches_expo, foto_maria
+from .views import get_coches, get_expo, get_coches_expo, foto_maria, get_items_expo
 
 urlpatterns = [
     path('coches/', get_coches, name='get_coches'),
     path('expo/', get_expo, name='get_expo'),
     path('foto/', foto_maria, name='foto_maria'),
     path('coches_expo/',get_coches_expo, name='get_coches_expo'),
+    path('coches_expo/<int:id_expo>/', get_items_expo, name='get_items_expo')
 ]

fba3110901262119c4efaf092117afd73547758d
Miguel Ángel López González <miguelangellopezgonzalez@MacBook-Air-de-Miguel.local>
2026-04-27 16:13:23 +0200

    Cosas

--- uxia/src/components/Header.jsx
+++ uxia/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import uxiaLogo from '../assets/uxiaLogo.png'
+import uxiaLogoBlack from '../assets/uxiaLogo-black.png'
 import ThemeButton from './ThemeButton'
 import { useTheme } from '../context/themeContext'
 
@@ -7,8 +8,8 @@ export default function Header() {
 
     return (
         <header className='w-full shadow-md'>
-            <div className={`max-w-[1440px] mx-auto flex items-center gap-4 p-4 sm:p-8 ${isDarkMode ? "bg-gray-800 text-white" : ""}`}>
-                <img src={uxiaLogo} className='w-20' alt="Logo d'uxia"/>
+            <div className={`max-w-full mx-auto flex items-center gap-4 p-4 sm:p-8 ${isDarkMode ? "bg-gray-800 text-white" : ""}`}>
+                <img src={isDarkMode ? uxiaLogoBlack : uxiaLogo} className='w-20' alt="Logo d'uxia"/>
                 <p className="font-semibold">UXIA</p>
                 <ThemeButton 
                     isDarkMode={isDarkMode} 

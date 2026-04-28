7d195d3df3e13dbebd0d21453e47d31d6239187f
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-04-28 19:32:59 +0200

    problemas con login

--- uxia/src/pages/AdminLogin.jsx
+++ uxia/src/pages/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import config from "../config"
 
 export default function AdminLogin() {
   const [user, setUser] = useState("");
